78a4d64234fe60cd1b2ea254d17217f267f32809
MaartenLangen <45261990+MaartenLangen@users.noreply.github.com>
2021-12-02 21:22:19 +0100

    script for HPC
    - some bug fixes in all the code
    - changed the way pairs of cell types are calculated.
    - creation of annotation matrix pulled out of for loop and optimized
    - only idea analyses of genes of interest are done, as otherwise to much time.

--- IBP-script.R
+++ IBP-script.R
@@ -1,21 +1,3 @@
-# Install the packages
-# install.packages("devtools")
-library("devtools")
-# install_github("nghiavtr/BPSC")
-
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# BiocManager::install("SingleCellExperiment")
-# BiocManager::install("biomaRt")
-# BiocManager::install("MAST")
-
-# install.packages("remotes")
-# remotes::install_github("davismcc/scater")
-# install.packages('scuttle')
-# install.packages('pheatmap')
-
-
-# Load the packages
 library("BPSC")
 library("SingleCellExperiment")
 library("scater")
@@ -24,42 +6,35 @@ library("Matrix")
 library('scuttle')
 library("pheatmap")
 library("MAST")
+library("iDEA")
 
-# Load the data
+load("workspace.RData")
 
-# X..MatrixMarket references gene in excel file, matrix is replicate, coordinate
-# is count
-gene_by_cell_count_matrix <-
-  read.csv("GSE126954_gene_by_cell_count_matrix.txt", sep = "")[,1:3]
+gene_by_cell_count_matrix <- read.csv("GSE126954_gene_by_cell_count_matrix.txt", sep = "")[,1:3]
 
-# load gene and cell annotations
 gene_annotation <- read.csv("GSE126954_gene_annotation.csv")[,2:3]
-cell_annotation <- read.csv("GSE126954_cell_annotation.csv")[,-1] 
+cell_annotation <- read.csv("GSE126954_cell_annotation.csv")[,-1]
 
-cells_bin = cell_annotation[cell_annotation$raw.embryo.time.bin=="330-390",]
-
-# generate matrix where row=gene, column=replicate
 data_matrix = sparseMatrix(i=gene_by_cell_count_matrix$X..MatrixMarket,
                           j=gene_by_cell_count_matrix$matrix,
                           x=gene_by_cell_count_matrix$coordinate
                            )
-
 rownames(data_matrix)=gene_annotation[,1]
 colnames(data_matrix)=cell_annotation[,1]
 
-# select a small part of data (the first 1000 cells in the matrix within the selected time bin)
-data_matrix = data_matrix[,as.numeric(unlist(rownames(cells_bin)))][,1:1000]
+head(data_matrix)
+dim(data_matrix)
 
-# create SCE object
+cells_bin = cell_annotation[cell_annotation$raw.embryo.time.bin=="330-390",]
+data_matrix = data_matrix[,as.numeric(unlist(rownames(cells_bin)))]
+
 sce <- SingleCellExperiment(
   assays = list(counts = data_matrix),
   rowData = data.frame(gene_names = rownames(data_matrix)),
   colData = data.frame(cell_names = colnames(data_matrix))
 )
 
-# Cleaning the dataset ---------------------------------------------------------
-
-## Remove genes that are not expressed in any cell -----------------------------
+## Remove genes that are not expressed in any cell
 keep_feature <- rowSums(counts(sce) > 0) > 0
 sce_filtered <- SingleCellExperiment(
   assays = list(counts = counts(sce)[keep_feature, ]),
@@ -67,18 +42,13 @@ sce_filtered <- SingleCellExperiment(
   colData = data.frame(cell_names = colData(sce))
 )
 
-## Quality control: ------------------------------------------------------------
 sce_filtered <- addPerCellQC(sce_filtered)
 sce_filtered <- addPerFeatureQC(sce_filtered)
-
 hist(sce_filtered$total, breaks = 100) #hist of counts per cell
 
 unique_genes_per_cell <- colSums(counts(sce)>0)
-hist(unique_genes_per_cell,breaks=100)
+hist(unique_genes_per_cell,breaks=100) #hist of unique genes per cell
 
-# TODO: make thresholds based on histograms
-
-### Normalize data by CPM and get the log transformed counts 
 cpm(sce_filtered) <- calculateCPM(sce_filtered) # using scater package
 sce_filtered <- logNormCounts(sce_filtered,transform="log") 
 # this logNormCounts function comes from scuttle package. Do not change sce_filtered into logcounts(sce_filtered) although that seems more logical.
@@ -89,36 +59,21 @@ sce_filtered <- logNormCounts(sce_filtered,transform="log")
 sce_cpm <- cpm(sce_filtered) # CPM for BPSC input
 sce_log <- sce_filtered@assays@data@listData$logcounts # log transformed counts for heatmap input
 
-
-### Generate the column indices of the gene expression matrix corresponding to each cell type 
-
-# First, generate the cell types included in the matrix being analyzed 
-# and make them correspond to the cell names (column names of the matrix)
-# cell_annotation[,6] contains the cell types we need.
-# We use them to split the data into groups containing gene expression of different cell types
-# dimnames(sce_cpm)[[2]] contains the column names of the sparse matrix
 cell_type <- cell_annotation[cell_annotation$cell %in% dimnames(sce_cpm)[[2]],6] 
 
-# Unique values in the vector cell_type that is not NA
-# We do not want to take the type-unidentified (na) cells into the differential expression analysis
-unique_cell_types <- unique(cell_type)[!is.na(unique(cell_type))] # we do not take the type-unidentified (na) cells into the differential expression analysis
+unique_cell_types <- unique(cell_type)[!is.na(unique(cell_type))]
 unique_cell_types
 length(unique_cell_types)
 
-# Create a list for cell indices
 cell_indices <- list()
 # Extract the cell indices according to each of the cell type and put them in the list
 for (i in 1:length(unique_cell_types)){
   cell_indices[[unique_cell_types[i]]] <- dimnames(sce_filtered[,which(cell_type==unique_cell_types[i])])[[2]]
 }
-# We can retrieve the cell indices of a cell type  by either the index of the list, or the cell type such as 'Body_wall_muscle'
+
 cell_indices[[1]][1:5]
 cell_indices[['Body_wall_muscle']][1:5]
 
-
-
-### Heatmap
-# Preparation for the heatmap
 matList <- list()
 cellTypeList <- list()
 
@@ -140,11 +95,8 @@ colnames(cellType) <- "Cell type"
 mat.whole <- data.frame(as.matrix(mat.whole))
 rownames(cellType) <- colnames(mat.whole)
 
-# Show heatmap
 pheatmap(mat.whole,annotation=cellType,cluster_cols=FALSE)
 
-
-### Use MAST to compute logFC based on the raw counts
 control.countData=sce_filtered[1:20,cell_indices[[1]]]
 treated.countData=sce_filtered[1:20,cell_indices[[2]]]
 countData <- cbind(control.countData,treated.countData)
@@ -153,23 +105,21 @@ cellType <- as.data.frame(cellType)
 colnames(cellType) <- "CellType"
 colData(countData) <- cbind(colData(countData),cellType)
 head(colData(countData),3)
+
 colData(countData)$CellType<-factor(colData(countData)$CellType)
 countData<-SceToSingleCellAssay(countData, class = "SingleCellAssay",check_sanity = FALSE) #uses MAST package
 zlmCond <- zlm(~CellType, countData)
 logFC<-getLogFC(zlmCond)[,c(1,3)]
 
-### Differential expression analysis using BPSC 
-# (Here we take only the first 20 genes to run a quick small test because BPSC takes a bit long time to run)
-
-## A test to compare the gene expression of only the first two cell types: "Body_wall_muscle" and "Ciliated_amphid_neuron"
-#Define the two groups to be compared
 control.mat=sce_cpm[1:20,cell_indices[[1]]]
 treated.mat=sce_cpm[1:20,cell_indices[[2]]]
+
 #Create a data set by merging the control group and the treated group
 bp.mat=cbind(control.mat,treated.mat)
 rownames(bp.mat)=c(1:nrow(bp.mat))
 colnames(bp.mat)=c(1:ncol(bp.mat))
 group=c(rep(1,ncol(control.mat)),rep(2,ncol(treated.mat)))
+
 #First, choose IDs of all cells of the control group for estimating parameters of BP models
 controlIds=which(group==1)
 #Create a design matrix including the group labels. All batch effects can be also added here if they are available
@@ -178,9 +128,11 @@ design=model.matrix(~group)
 coef=2 
 #Run BPglm for differential expression analysis
 res=BPglm(data=bp.mat, controlIds=controlIds, design=design, coef=coef, estIntPar=FALSE, useParallel=FALSE) 
+
 #Plot the p-value distribution
 res$PVAL
 hist(res$PVAL, breaks=20)
+
 #Summarize the resutls
 summary(res)
 
@@ -190,16 +142,9 @@ results <- cbind(logFC,res$PVAL)
 colnames(results) <- c('gene','logFC','P-value')
 results
 
-
-
-### Code that may be useful later on
 # Use biomRT package to get access to WormBase ParaSite BioMart
 # Establish a connection to the WormBase ParaSite BioMart
 mart <- useMart("parasite_mart", dataset = "wbps_gene", host = "https://parasite.wormbase.org", port = 443)
-
-# List available filters that can be used in a query and attributes you can retrieve
-listFilters(mart)[1:100,]
-listAttributes(mart)[1:100,]
 
 # Retrieve the GO terms
 gene_info <- getBM(filters="wbps_gene_id", 
@@ -207,76 +152,115 @@ gene_info <- getBM(filters="wbps_gene_id",
   values=row.names(data_matrix), 
   mart=mart,
   uniqueRows=FALSE)
+
 dim(gene_info) 
+
 head(gene_info)
 
-
-
-
-###iDEA
-#calculate variance
 #Here the first results(only two cell types) is used!!
 pvalue <- results$`P-value` #### the pvalue column
 zscore <- qnorm(pvalue/2.0, lower.tail=FALSE) #### convert the pvalue to z-score
 fc <- results$logFC ## the fold change column
 se_beta <- abs(fc/zscore) ## to approximate the standard error of beta
 var = se_beta^2  ### square 
-summary = data.frame(fc = fc,variance = var,row.names = results$gene)# Summary is a matrix of fold change and variance of each gene
-#get annotation data
-#know how many go terms we have
+
+# Summary is a matrix of fold change and variance of each gene
+summary = data.frame(fc = fc,variance = var,row.names = results$gene)
+
+# get annotation data
+# know how many go terms we have
 length(unique(gene_info$go_name))#6907
-annotation<-matrix(0,nrow =nrow(summary) ,ncol = 6907)
-rownames(annotation)<-results$gene
-colnames(annotation)<-unique(gene_info$go_name_1006)
-for (i in 1:nrow(annotation)) {
-  for (j in 1:ncol(annotation)) {
-    index<-which(gene_info$wbps_gene_id==rownames(annotation)[i])
-    for (k in 1:length(index)) {
-      if(gene_info$go_name_1006[k]==colnames(annotation)[j]) {
-        annotation[i,j]=1
+if ("annotation.RData" %in% dir()){
+    load(file="annotation.RData")
+} else {
+    annotation<-matrix(0,nrow =nrow(summary) ,ncol = 6907)
+    rownames(annotation)<-results$gene
+    colnames(annotation)<-unique(gene_info$go_name_1006)
+    for (i in 1:nrow(annotation)) {
+      for (j in 1:ncol(annotation)) {
+        index<-which(gene_info$wbps_gene_id==rownames(annotation)[i])
+        for (k in 1:length(index)) {
+          if(gene_info$go_name_1006[k]==colnames(annotation)[j]) {
+            annotation[i,j]=1
+          }
+        }
       }
-      
     }
-  }
-  
+    save(annotation, file="annotation.RData")
 }
 
-#install iDEA package
-devtools::install_github('xzhoulab/iDEA')
-library(iDEA)
 #create idea object
 idea<-CreateiDEAObject(summary,annotation)
+
 #Fit the model
+options(mc.cores = 1) # to fix an error. Has to do with problems in parallelization 
 idea <- iDEA.fit(idea)
+
 #correct p-values
 idea <- iDEA.louis(idea)
+
 #get output
 idea@gsea
+
 #DE analysis of individual gene
 #with pre-selected genes
 pip = unlist(idea@de[["membrane"]]$pip)
 head(pip)
+
 #without pre-selected genes
 idea <- iDEA.BMA(idea)
 head(idea@BMA_pip)
 
+save.image(file="workspace.RData")
 
-## Generalize the small test to all possible combinations of cell types
-# Generate all combinations of numbers from 1 to the number of cell types, taken 2 at a time
-combinations <- expand.grid(1:length(unique_cell_types), 1:length(unique_cell_types))
-head(combinations)
-combinations <- combinations[combinations$Var1 != combinations$Var2,]
-head(combinations)
-nrow((combinations)) # number of combinations
+genes_of_interest = as.matrix(read.delim("genes_of_interest.txt", head=FALSE))
+genes_index = which(gene_annotation[,2] %in% genes_of_interest)
+geneIDs = gene_annotation[genes_index,1]
 
-#Until now it's just for comparison of two cell types, so the following code is for adding the iDea analysis to the "for loop" for several pair-wise comparisons.
-# Create a list for log fold change + p-value from BPSC results
+combinations = data.frame()
+for (i in 1:(length(unique_cell_types)-1)){
+    for (j in (i+1):length(unique_cell_types)){
+        row = c(i,j)
+        combinations = rbind(combinations, row)
+    }
+}
+
+annotation = sparseMatrix(i=nrow(sce_cpm),
+                          j=length(unique(gene_info$go_name)),
+                          x=0
+                           )
+rownames(annotation)<-rownames(sce_cpm)
+colnames(annotation)<-unique(gene_info$go_name_1006)
+
+for (i in 1:nrow(annotation)) {
+    if (i%%100==0) i
+    index<-which(gene_info$wbps_gene_id==rownames(annotation)[i])
+    if (length(index)==0) next
+    for (j in 1:ncol(annotation)) {
+      for (k in 1:length(index)) {
+        if(gene_info$go_name_1006[k]==colnames(annotation)[j]) {
+          annotation[i,j]=1
+        }
+      }
+    }
+}
+
+annotation_matrix = as.matrix(annotation)
+annotation_df = as.data.frame(annotation_matrix)
+annotation = annotation_df
+
+# Create a list for log fold change + p-value from BPSC results and for iDEA results
 results <- list()
-results_iDEA<-list()
+list_of_idea = list()
+
+options(mc.cores = 1) # to fix an error. Has to do with problems in parallelization in idea
+
 for (i in 1:nrow(combinations)){
+  print(i)
+  startTime = Sys.time()
   #Define the two groups to be compared (Remember of remove 1:20 when we run it for the whole data containing all the cells in a time bin!!!)
-  control.mat=sce_cpm[1:20,cell_indices[[combinations[i,1]]]]
-  treated.mat=sce_cpm[1:20,cell_indices[[combinations[i,2]]]]
+  control.mat=sce_cpm[geneIDs,cell_indices[[combinations[i,1]]]]
+  treated.mat=sce_cpm[geneIDs,cell_indices[[combinations[i,2]]]]
   #Create a data set by merging the control group and the treated group
   bp.mat=cbind(control.mat,treated.mat)
   rownames(bp.mat)=c(1:nrow(bp.mat))
@@ -286,67 +270,48 @@ for (i in 1:nrow(combinations)){
   res=BPglm(data=bp.mat, controlIds=which(lapply(group, as.numeric)==1), design=model.matrix(~group), coef=2, estIntPar=FALSE, useParallel=FALSE)
   
   # Use MAST to compute logFC based on the raw counts
-  control.countData=sce_filtered[1:20,cell_indices[[combinations[i,1]]]]
-  treated.countData=sce_filtered[1:20,cell_indices[[combinations[i,2]]]]
+  control.countData=sce_filtered[geneIDs,cell_indices[[combinations[i,1]]]]
+  treated.countData=sce_filtered[geneIDs,cell_indices[[combinations[i,2]]]]
   countData <- cbind(control.countData,treated.countData)
   cellType <- as.data.frame(rep(c(1,2), c(ncol(control.countData),ncol(treated.countData))))
   colnames(cellType) <- "CellType"
   colData(countData) <- cbind(colData(countData),cellType)
   colData(countData)$CellType<-factor(colData(countData)$CellType)
   countData<-SceToSingleCellAssay(countData, class = "SingleCellAssay",check_sanity = FALSE)
-  zlmCond <- zlm(~cellType, countData)
+  zlmCond <- zlm(~CellType, countData)
   logFC<-getLogFC(zlmCond)[,c(1,3)]
-  
+    
   # Log fold change + p-value from BPSC
   PVAL<-as.data.frame(res$PVAL)
   result <- cbind(logFC,res$PVAL)
-  colnames(results) <- c('gene','logFC','P-value')
+  colnames(result) <- c('gene','logFC','P-value')
   results[[paste(unique_cell_types[combinations[i,1]],unique_cell_types[combinations[i,2]], sep=" vs. ")]]=result
   
   ###iDEA
   #calculate variance
   #Here the first results(only two cell types) is used!!
-  pvalue <- results$`P-value` #### the pvalue column
+  pvalue <- result$`P-value` #### the pvalue column
   zscore <- qnorm(pvalue/2.0, lower.tail=FALSE) #### convert the pvalue to z-score
-  fc <- results$logFC ## the fold change column
+  fc <- result$logFC ## the fold change column
   se_beta <- abs(fc/zscore) ## to approximate the standard error of beta
   var = se_beta^2  ### square 
-  summary = data.frame(fc = fc,variance = var,row.names = results$gene)# Summary is a matrix of fold change and variance of each gene
-  #get annotation data
-  #know how many go terms we have
-  length(unique(gene_info$go_name))#6907
-  annotation<-matrix(0,nrow =nrow(summary) ,ncol = 6907)
-  rownames(annotation)<-results$gene
-  colnames(annotation)<-unique(gene_info$go_name_1006)
-  for (i in 1:nrow(annotation)) {
-    for (j in 1:ncol(annotation)) {
-      index<-which(gene_info$wbps_gene_id==rownames(annotation)[i])
-      for (k in 1:length(index)) {
-        if(gene_info$go_name_1006[k]==colnames(annotation)[j]) {
-          annotation[i,j]=1
-        }
-        
-      }
-    }
-    
-  }
+  summary = data.frame(fc = fc,variance = var,row.names = result$gene)# Summary is a matrix of fold change and variance of each gene
   
-  #install iDEA package
-  devtools::install_github('xzhoulab/iDEA')
-  library(iDEA)
   #create idea object
-  idea<-CreateiDEAObject(summary,annotation)
+  idea<-CreateiDEAObject(summary,annotation[geneIDs,])
   #Fit the model
   idea <- iDEA.fit(idea)
   #correct p-values
   idea <- iDEA.louis(idea)
-  #get output
-  idea@gsea
   #DE analysis of individual gene
   #with pre-selected genes
   pip = unlist(idea@de[["membrane"]]$pip)
-  head(pip)
   #without pre-selected genes
   idea <- iDEA.BMA(idea)
-  head(idea@BMA_pip)
+    
+  #Save idea to list, so we can do analysis later
+  list_of_idea <- cbind(list_of_idea,idea)
+  
+  stopTime = Sys.time()
+  print(stopTime-startTime)
 } 
